3e3515ab6af6ed39cd8ed882da197ec11bbd6839
Bonang Panji Nur <bopan86@gmail.com>
2025-11-28 17:31:50 +0700

    huhu:

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '97202f1fe870660a66b3');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '0fe46333dba2935acfe2');
